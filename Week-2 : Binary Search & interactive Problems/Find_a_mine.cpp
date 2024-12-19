d387da32409c757e7147aebd9b61100f431816d8
Rakib <rmvk187@gmail.com>
2024-12-19 18:58:15 +0530

    week-2 Binary Search & interactive problem | Problem-2

---
+++ Week-2 : Binary Search & interactive Problems/Find_a_mine.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+/*Problem Link -> */
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define Yes cout << "YES" << '\n'
+#define No cout << "NO" << '\n'
+#define nl '\n'
+#define loop(i, s, n) for (ll i = s; i <= n; i++)
+using namespace std;
+using namespace __gnu_pbds;
+using namespace chrono;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using pbdms = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/*Debug*/
+#ifdef Rakib_18
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void init_code() {
+#ifdef Rakib_18
+	freopen("Error.txt", "w", stderr);
+#endif
+}
+bool isPrime(ll n) {if (n <= 1)return false; if (n <= 3)return true; if (n % 2 == 0 || n % 3 == 0)return false; for (ll i = 5; i * i <= n; i += 6) {if (n % i == 0 || n % (i + 2) == 0)return false;} return true;}
+ll lcm(ll a, ll b) {return (a / __gcd(a, b)) * b;}
+int nXOR(int n) {if (n % 4 == 0)return n; if (n % 4 == 1)return 1; if (n % 4 == 2)return n + 1; return 0;}
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+int ask(int x, int y) {
+	cout << "? " << x << " " << y << endl;
+	cout.flush();
+	int res;
+	cin >> res;
+	return res;
+}
+void solve()
+{
+	int n, m;
+	cin >> n >> m;
+
+	int d = ask(1, 1);
+	int x1 = min(n, 1 + d), y1 = max(1, (d + 2 - x1));
+	int y2 = min(m, 1 + d), x2 = max(1, (d + 2 - y2));
+
+	int resd1 = ask(x1, y1);
+	int resd2 = ask(x2, y2);
+
+	if (resd1 == 0) {
+		cout << "! " << x1 << ' ' << y1 << endl;
+		cout.flush();
+		return;
+	}
+	if (resd2 == 0) {
+		cout << "! " << x2 << ' ' << y2 << endl;
+		cout.flush();
+		return;
+	}
+	int ansx2 = x2 + (resd2 / 2), ansy2 = y2 - (resd2 / 2);
+	int ansx1 = x1 - (resd1 / 2), ansy1 = y1 + (resd1 / 2);
+
+	if (ask(ansx1, ansy1) == 0) {
+		cout << "! " << ansx1 << ' ' << ansy1 << endl;
+		cout.flush();
+	}
+	else {
+		cout << "! " << ansx2 << ' ' << ansy2 << endl;
+		cout.flush();
+	}
+}
+int main()
+{
+	init_code();
+	fastio();
+	int t = 1;
+	cin >> t;
+	auto start1 = high_resolution_clock::now();
+	while (t--)
+	{
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef Rakib_18
+	cerr << "Time: " << duration . count() / 1000 << " ms" << endl;
+#endif
+	return 0;
+}
