50061f4b47b81abd3e259eb84a3dc81f58be7215
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2025-07-11 17:00:25 +0530

    Day - 5 | Orac and Lcm

---
+++ Week-6: Advanced Number Theory/Orac_and_Lcm.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define Yes cout << "YES" << '\n'
+#define No cout << "NO" << '\n'
+#define nl '\n'
+#define loop(i, s, n) for (ll i = s; i <= n; i++)
+#define sz(x) ((int)(x).size())
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using pbdms = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#ifdef Rakib_18
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const ll mod = 1e9 + 7;
+ll expo(ll a, ll b, ll mod) { ll res = 1; while (b > 0) { if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1; } return res; }
+ll mminvprime(ll a, ll b) { return expo(a, b - 2, b); }
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+bool isPrime(ll n) { if (n <= 1)return false; if (n <= 3)return true; if (n % 2 == 0 || n % 3 == 0)return false; for (ll i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0)return false; } return true; }
+ll lcm(ll a, ll b) { return (a / __gcd(a, b)) * b; }
+ll mod_add(ll a, ll b, ll m) { a = a % m; b = b % m; return (((a + b) % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+ll mod_sub(ll a, ll b, ll m) { a = a % m; b = b % m; return (((a - b) % m) + m) % m; }
+ll mod_div(ll a, ll b, ll m) { a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m; }  //only for prime m
+int nXOR(int n) { if (n % 4 == 0)return n; if (n % 4 == 1)return 1; if (n % 4 == 2)return n + 1; return 0; }
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+vector<int>ShortestPrimeFactor(int maxN) {
+    vector<bool>Prime(maxN, true);
+    vector<int>spf(maxN, 1e9);
+
+    for (ll i = 2; i < maxN;i++) {
+        if (Prime[i]) {
+            spf[i] = i;
+            for (ll j = i * i;j < maxN;j += i) {
+                Prime[j] = false;
+                spf[j] = min(spf[j], (int)i);
+            }
+        }
+    }
+    return spf;
+}
+void solve()
+{
+    int maxN = 2e5 + 1;
+    vector<int>spf = ShortestPrimeFactor(maxN);
+
+    int n;
+    cin >> n;
+    vector<int>a(n), primes[maxN];
+    for (auto& i : a)cin >> i;
+
+    for (auto i : a) {
+        while (i != 1) {
+            int p = spf[i];
+            int cnt = 0;
+            while (i % p == 0) {
+                cnt++;
+                i /= p;
+            }
+            primes[p].push_back(cnt);
+        }
+    }
+
+    ll ans = 1;
+    for (int i = 0;i < maxN;i++) {
+        sort(primes[i].begin(), primes[i].end());
+        if (primes[i].size() < n - 1)continue;
+        else {
+            if (primes[i].size() == n)ans *= expo(i, primes[i][1], mod);
+            else ans *= expo(i, primes[i][0], mod);
+        }
+    }
+    cout << ans << nl;
+}
+int32_t main()
+{
+    fastio();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
