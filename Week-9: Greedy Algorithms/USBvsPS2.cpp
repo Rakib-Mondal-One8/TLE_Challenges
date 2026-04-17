eac9899f5a7034ab692f3c590aa7746d19afb8e0
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-04-17 16:33:27 +0530

    Day - 2

---
+++ Week-9: Greedy Algorithms/USBvsPS2.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+/*Problem Link -> */
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define Yes cout << "YES" << '\n'
+#define No cout << "NO" << '\n'
+#define nl '\n'
+#define loop(i, s, n) for (ll i = s; i <= n; i++)
+#define sz(x) ((int)(x).size())
+using namespace std;
+using namespace __gnu_pbds;
+using namespace chrono;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using pbdms = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/*Debug*/
+#ifdef Rakib_18
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void init_code() {
+#ifdef Rakib_18
+	freopen("Error.txt", "w", stderr);
+#endif
+}
+const int mod = 1e9 + 7;
+// const int mod = 998244353;
+// const long long mod = 1375927501846395853LL;
+bool isPrime(ll n) { if (n <= 1)return false; if (n <= 3)return true; if (n % 2 == 0 || n % 3 == 0)return false; for (ll i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0)return false; } return true; }
+ll lcm(ll a, ll b) { return (a / __gcd(a, b)) * b; }
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+void solve()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	int m;
+	cin >> m;
+
+	priority_queue<ll, vector<ll>, greater<ll>>pq1, pq2;
+	for (int i = 1; i <= m; i++) {
+		ll x;
+		string port;
+		cin >> x >> port;
+
+		if (port == "USB")pq1.push(x);
+		else pq2.push(x);
+	}
+
+	ll cost = 0, cnt = 0;
+
+	while (a > 0 && !pq1.empty()) {
+		cost += pq1.top();
+		pq1.pop();
+		cnt++;
+		a--;
+	}
+	while (b > 0 && !pq2.empty()) {
+		cost += pq2.top();
+		pq2.pop();
+		cnt++;
+		b--;
+	}
+	while (!pq2.empty()) {
+		pq1.push(pq2.top());
+		pq2.pop();
+	}
+	while (c > 0 && !pq1.empty()) {
+		cost += pq1.top();
+		pq1.pop();
+		cnt++;
+		c--;
+	}
+	cout << cnt << " " << cost << nl;
+
+}
+int main()
+{
+	init_code();
+	fastio();
+	int t = 1;
+	// cin >> t;
+	auto start1 = high_resolution_clock::now();
+	while (t--)
+	{
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef Rakib_18
+	cerr << "Time: " << duration . count() / 1000 << " ms" << endl;
+#endif
+	return 0;
+}
