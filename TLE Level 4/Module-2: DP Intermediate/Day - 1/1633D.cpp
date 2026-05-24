0635bfaca87bea8c0399a2666b38c572ce8f6357
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-05-24 15:18:22 +0530

    DP Intermediate | Day - 1

---
+++ TLE Level 4/Module-2: DP Intermediate/Day - 1/1633D.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define sz(x) ((int)(x).size())
+#define nl '\n'
+#ifdef Rakib_18
+#include "debug.hpp"
+#else
+#define debug(...)
+#endif
+void init_code() {
+#ifdef Rakib_18
+	freopen("Error.txt", "w", stderr);
+#endif
+}
+using namespace chrono;
+
+/*_________________________________________________________________________________________________________________________________________________________________________________________________________________________*/
+const int mod = 1e9 + 7;
+int expo(int a, int b, int mod) { int res = 1; while (b > 0) { if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1; } return res; }
+int mminvprime(int a, int b) { return expo(a, b - 2, b); }
+int inv(int i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+bool isPrime(int n) { if (n <= 1)return false; if (n <= 3)return true; if (n % 2 == 0 || n % 3 == 0)return false; for (int i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0)return false; } return true; }
+int lcm(int a, int b) { return (a / __gcd(a, b)) * b; }
+int mod_add(int a, int b, int m) { a = a % m; b = b % m; return (((a + b) % m) + m) % m; }
+int mod_mul(int a, int b, int m) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+int mod_sub(int a, int b, int m) { a = a % m; b = b % m; return (((a - b) % m) + m) % m; }
+int mod_div(int a, int b, int m) { a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m; }  //only for prime m
+int nXOR(int n) { if (n % 4 == 0)return n; if (n % 4 == 1)return 1; if (n % 4 == 2)return n + 1; return 0; }
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+/*_________________________________________________________________________________________________________________________________________________________________________________________________________________________*/
+const int N = 1e3 + 5;
+vector<int>reachOp(N, 1e9);
+
+void go() {
+	reachOp[1] = 0;
+	for (int i = 1; i < N; i++) {
+		for (int x = 1; x <= i; x++) {
+			int j = i + i / x;
+
+			if (j < N)reachOp[j] = min(reachOp[j], reachOp[i] + 1);
+		}
+	}
+}
+
+void RakibOne8()
+{
+	int n, k;
+	cin >> n >> k;
+
+	vector<int>b(n), c(n);
+	for (auto &x : b)cin >> x;
+	for (auto &x : c)cin >> x;
+
+	/*
+	dp[i][j] = maximum value can get starting from i to n-1 such that j operations are left
+	dp[i][j] = max(c[i] + dp[i+1][j-reachOp[i]] or dp[i+1][j]);
+	*/
+	k = min(k, 12 * n);
+
+	vector<vector<int>>dp(n + 1, vector<int>(k + 1));
+	for (int i = 0; i < n; i++)dp[i][0] = 0;
+
+	for (int i = n - 1; i >= 0; i--) {
+		for (int j = 0; j <= k; j++) {
+			int pick = 0;
+			if (j >= reachOp[b[i]])pick = c[i] + dp[i + 1][j - reachOp[b[i]]];
+			int skip = dp[i + 1][j];
+
+			dp[i][j] = max(pick, skip);
+		}
+	}
+
+	cout << dp[0][k] << nl;
+
+
+}
+int32_t main()
+{
+	init_code();
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int t = 1;
+	cin >> t;
+	go();
+	auto start1 = high_resolution_clock::now();
+	while (t--)
+	{
+		RakibOne8();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef Rakib_18
+	cerr << "Time: " << duration . count() / 1000 << " ms" << endl;
+#endif
+	return 0;
+}
