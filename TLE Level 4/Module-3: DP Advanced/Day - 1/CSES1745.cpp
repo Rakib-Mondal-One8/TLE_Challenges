6ad8a91d061e5f49785167ce457989cd186dc518
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-06-05 16:07:11 +0530

    Day - 1

---
+++ TLE Level 4/Module-3: DP Advanced/Day - 1/CSES1745.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define sz(x) ((int)(x).size())
+#define nl '\n'
+#ifdef Rakib_18
+#include "debug.hpp"
+#else
+#define debug(...)
+#endif
+void init_code() {
+#ifdef Rakib_18
+	freopen("Error.txt", "w", stderr);
+#endif
+}
+using namespace chrono;
+
+/*_________________________________________________________________________________________________________________________________________________________________________________________________________________________*/
+const int mod = 1e9 + 7;
+int expo(int a, int b, int mod) { int res = 1; while (b > 0) { if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1; } return res; }
+int mminvprime(int a, int b) { return expo(a, b - 2, b); }
+int inv(int i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+bool isPrime(int n) { if (n <= 1)return false; if (n <= 3)return true; if (n % 2 == 0 || n % 3 == 0)return false; for (int i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0)return false; } return true; }
+int lcm(int a, int b) { return (a / __gcd(a, b)) * b; }
+int mod_add(int a, int b, int m) { a = a % m; b = b % m; return (((a + b) % m) + m) % m; }
+int mod_mul(int a, int b, int m) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+int mod_sub(int a, int b, int m) { a = a % m; b = b % m; return (((a - b) % m) + m) % m; }
+int mod_div(int a, int b, int m) { a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m; }  //only for prime m
+int nXOR(int n) { if (n % 4 == 0)return n; if (n % 4 == 1)return 1; if (n % 4 == 2)return n + 1; return 0; }
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+/*_________________________________________________________________________________________________________________________________________________________________________________________________________________________*/
+
+void RakibOne8()
+{
+	int n;
+	cin >> n;
+
+	vector<int>a(n);
+	for (auto &x : a)cin >> x;
+
+	int sum = accumulate(a.begin(), a.end(), 0LL);
+
+	const int INF = 1e9;
+	vector<vector<int>>dp(n + 1, vector<int>(sum + 2, -INF));
+	/*
+	dp[i][j] = number of ways to make total sum j using i to n-1 elements
+	*/
+	dp[n][0] = 1;
+
+	for (int i = n - 1; i >= 0; i--) {
+		for (int j = 0; j <= sum; j++) {
+			int pick = -INF;
+			if (j >= a[i])
+				pick = dp[i + 1][j - a[i]];
+			int skip = dp[i + 1][j];
+
+			dp[i][j] = max(pick, skip);
+		}
+	}
+
+	set<int>ans;
+	for (int i = 1; i <= sum; i++) {
+		if (dp[0][i] > 0 )ans.insert(i);
+	}
+
+	cout << ans.size() << nl;
+	for (auto e : ans)cout << e << " ";
+	cout << nl;
+
+
+}
+int32_t main()
+{
+	init_code();
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int t = 1;
+	// cin >> t;
+	auto start1 = high_resolution_clock::now();
+	while (t--)
+	{
+		RakibOne8();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef Rakib_18
+	cerr << "Time: " << duration . count() / 1000 << " ms" << endl;
+#endif
+	return 0;
+}
